f6873ddd0ff3a8c23e19d120695d835714df1531
dogusmiuw <dceliknodes@gmail.com>
2025-09-06 00:00:00 +0300

    feat: create auth middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+exports.isAuthenticated = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res
+            .status(401)
+            .json({message: "No token provided. User not authenticated."});
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = {id: decoded.id, role: decoded.role};
+
+        next();
+    } catch (err) {
+        return res.status(401).json({message: "Invalid or expired token"});
+    }
+};
