726ebbf82464dadd6ebe97527cd602637b4b3e28
dogusmiuw <dceliknodes@gmail.com>
2025-09-06 00:06:24 +0300

    feat: add necessary user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,18 @@ const router = express.Router();
 const {deactivateUser} = require("../controllers/userController");
 const {isAuthenticated} = require("../middlewares/authMiddleware");
 const {isModerator} = require("../middlewares/roleMiddleware");
+const {
+    getMyProfile,
+    updateMyProfile,
+} = require("../controllers/userController");
 
+// user
+router.get("/", isAuthenticated, getAllUsers);
+router.get("/me", isAuthenticated, getMyProfile);
+router.put("/me", isAuthenticated, updateMyProfile);
+router.delete("/me", isAuthenticated, deleteMyAccount);
+
+// moderator
 router.patch("/:id/deactivate", isAuthenticated, isModerator, deactivateUser);
 
 module.exports = router;
